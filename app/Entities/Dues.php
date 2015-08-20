82d8ccb1ec562ebeb590d263174c59fb3b68e833
Anwar Sarmiento Ramos <anwarsarmiento@gmail.com>
2015-08-20 02:36:40 -0600

    modelo dues

--- app/Entities/Dues.php
+++ app/Entities/Dues.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Dues extends Model
 {
     protected $table = "affiliate_record_percentage";
+
+    public function Affiliates()
+    {
+        return $this->belongsTo(Dues::getClass(),'Affiliate_id','id');
+    }
 }
