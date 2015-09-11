610986a3f12428328c018ad0667ca0641c4d70aa
Francisco Gamonal <hfgamonalb@gmail.com>
2015-09-11 16:00:30 -0500

    fixed prom. reports

--- resources/views/affiliates/report/private/firm.blade.php
+++ resources/views/affiliates/report/private/firm.blade.php
@@ -1,3 +1,3 @@
-<p style="margin:0;font-size: 14px;">Sueldo Promedio: {{ number_format( ($dataPrivate[count($dataPrivate)-1][3]/$dataPrivate[count($dataPrivate)-1][2]), 2, '.', ',') }} </p>
+<p style="margin:0;font-size: 14px;">Sueldo Promedio: {{ number_format( ($dataPrivate[count($dataPrivate)-1][1]/$dataPrivate[count($dataPrivate)-1][2]), 2, '.', ',') }} </p>
 <p style="margin:0;font-size: 14px;">Cuotas por pagar: {{ $dues_total_max - $dataPrivate[count($dataPrivate)-1][2] }} </p>
 <p style="margin:0;font-size: 14px;">Cuotas pagadas: {{ $dataPrivate[count($dataPrivate)-1][2] }} de {{ $dues_total_max }}. </p>
--- resources/views/affiliates/report/salary-affiliate/firm.blade.php
+++ resources/views/affiliates/report/salary-affiliate/firm.blade.php
@@ -1,7 +1,7 @@
 <p style="font-size: 14px;margin: 0;">Total Sueldos D. Cont.: {{ number_format($dataAffiliate[count($dataAffiliate)-1][1],2,'.',',') }}</p>
-<p style="font-size: 14px;margin: 0;">Sueldo Promedio Cont.: {{ number_format( ($dataAffiliate[count($dataAffiliate)-1][3]/$dataAffiliate[count($dataAffiliate)-1][2]), 2, '.', ',') }} </p>
+<p style="font-size: 14px;margin: 0;">Sueldo Promedio Cont.: {{ number_format( ($dataAffiliate[count($dataAffiliate)-1][1]/$dataAffiliate[count($dataAffiliate)-1][2]), 2, '.', ',') }} </p>
 <p style="font-size: 14px;margin: 0 0 1em 0;">Total Pagado Cont.: {{ number_format($dataAffiliate[count($dataAffiliate)-1][3],2,'.',',') }} </p>
 <p style="font-size: 14px;margin: 0;">Total Sueldos D. 10%.: {{ number_format($dataPrivate[count($dataPrivate)-1][1],2,'.',',') }}</p>
-<p style="font-size: 14px;margin: 0;">Sueldo Promedio 10%.: {{  number_format( ($dataPrivate[count($dataPrivate)-1][3]/$dataPrivate[count($dataPrivate)-1][2]), 2, '.', ',') }}</p>
+<p style="font-size: 14px;margin: 0;">Sueldo Promedio 10%.: {{  number_format( ($dataPrivate[count($dataPrivate)-1][1]/$dataPrivate[count($dataPrivate)-1][2]), 2, '.', ',') }}</p>
 <p style="font-size: 14px;margin: 0;">Total Pagado 10%.: {{ number_format($dataPrivate[count($dataPrivate)-1][3],2,'.',',') }}</p>
 {{ destroyPage() }}
